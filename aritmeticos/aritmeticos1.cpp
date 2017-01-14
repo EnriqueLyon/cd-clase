18e8594c0355464eefec129a6ee95ea9e9ef76ec
EnriqueLyon <enriquelyon.ciclos@gmail.com>
2017-01-14 18:07:57 +0100

    Add files via upload

---
+++ aritmeticos/aritmeticos1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbio.h>
+
+int main(){
+
+	int nLista, fila =1, columna;
+
+	printf("introduzca el numero de lista del alumno:\n");
+	scanf("%1", %nLista);
+while(nLista > 17){
+		nLista -= 17;
+		fila++;
+	}
+	
+	columna = nLista;
+	
+	printf("El alumno se sienta en la fila %i y en la columna %i\n", fila, columna);
+
+	return EXIT_SUCCESS;
+}
