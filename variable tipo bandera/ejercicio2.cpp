95e8152daabb6850a4b2f6f5e37e285190a61cd6
EnriqueLyon <enriquelyon.ciclos@gmail.com>
2017-01-14 17:59:16 +0100

    Add files via upload

---
+++ variable tipo bandera/ejercicio2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+
+int nIntroducido=0 , nMayor = 0;
+
+print("Dime diez  número:\n ");
+for(int i= o, i < 10; i++){
+      printf("Numero %i",i+1);
+      scanf("&i", %nIntroducido);
+
+      if(nIntroducido >nMayor)
+	nMayor = nIntroducido;
+}
+printf("El número mas alto es el &i.\n"nMayor);
+
+return EXIT_SUCCES;
+}
---
+++ variable tipo bandera/ejercicio3.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+int nIntroducido=0, nMenor = 0;
+
+print("Dime diez números:\n");
+for(int i=0 , i< 10;i++){
+   printf("Numero &i", i+1);
+   scanf("&i", %nIntroducido);
+   
+ if(nIntroducido >nMayor)
+    nMayor = nIntroducido;
+}
+printf("El numero mas bajo es el %i.\n" nMneor);
+
+return EXIT_SUCCES;
+}
