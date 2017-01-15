1b66eb2dabac14d024831604e6376728a8a290c6
EnriqueLyon <enriquelyon.ciclos@gmail.com>
2017-01-15 18:16:34 +0100

    Add files via upload

---
+++ bits/bits2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+	int numB10;
+
+	printf("Introduce un número en base decimal:\n");
+	scanf(" %i", &numB10);
+
+	printf("el numero %i en base decimal pasa a ser %X en hexadecimal\n",numbB10,numB10);
+	
+	return EXIT_SUCCESS;
+
