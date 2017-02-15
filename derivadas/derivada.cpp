c8a45d66470ed946b8a4704efd540c920fa028c6
EnriqueLyon <enriquelyon.ciclos@gmail.com>
2017-02-15 17:44:32 +0100

    Add files via upload

---
+++ derivadas/derivada.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+//HACER UNA DERIVADA
+double x_cuad(double_*){
+return x*x;
+}
+double diferencia(double op1, double op2, double op3, double op4){
+return (op1 - op2)/(op3 - op4);
+}
+int main(int argc, char *argv[]{
+
+double x;
+titulo();
+printf("\n\tx: ");
+scanf
+printf("Introduce el valor de la variable")                
+        return EXIT_SUCCESS;
+}
+
